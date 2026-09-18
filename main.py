ae752870a4e7050392ca10396052277a6d70110e
Arda DİK <tcardadik@gmail.com>
2026-09-18 19:47:12 +0300

    Implement weather check and SMS notification
    Added functionality to check weather forecast and send SMS if rain is expected.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import requests
 from dotenv import load_dotenv
 from twilio.rest import Client
 
+# Load from ".env"
 load_dotenv()
 
+# Get API and personal informations from ".env"
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = os.getenv("OWM_API_KEY")
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
@@ -13,6 +15,7 @@ auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 twilio_number = os.getenv("TWILIO_NUMBER")
 my_number = os.getenv("MY_PHONE_NUMBER")
 
+#Set Params
 weather_params = {
     "lat": 41.042381,
     "lon": 28.912149,
@@ -21,15 +24,18 @@ weather_params = {
 }
 
 will_rain = False
+
+#Threw API a request and convert the data into json
 response = requests.get(OWM_endpoint, params=weather_params)
 response.raise_for_status()
 data = response.json()
 
+#Check if there is raining 12 hours ahead
 for hour_data in data["list"]:
     condition_id = hour_data["weather"][0]["id"]
     if int(condition_id) < 700:
         will_rain = True
-
+#If there is, sent SMS
 if will_rain:
     client = Client(account_sid, auth_token)
     message = client.messages.create(
