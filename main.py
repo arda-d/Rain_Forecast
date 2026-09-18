383f3ece0812a9e4462a66b093e65bb085877f94
kokgeyik <tcardadik@gmail.com>
2026-09-18 19:39:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.getenv("OWM_API_KEY")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
+twilio_number = os.getenv("TWILIO_NUMBER")
+my_number = os.getenv("MY_PHONE_NUMBER")
+
+weather_params = {
+    "lat": 41.042381,
+    "lon": 28.912149,
+    "appid": api_key,
+    "cnt": 4
+}
+
+will_rain = False
+response = requests.get(OWM_endpoint, params=weather_params)
+response.raise_for_status()
+data = response.json()
+
+for hour_data in data["list"]:
+    condition_id = hour_data["weather"][0]["id"]
+    if int(condition_id) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's raining today!",
+        from_=twilio_number,
+        to=my_number
+    )
+    print(message.status)
